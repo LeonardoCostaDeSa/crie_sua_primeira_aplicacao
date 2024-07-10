8875b1bf04ed5b6f8bcd704663681f29aa7c3e4c
Leonardo de Sa <leonardocostasa95@gmail.com>
2024-07-09 21:43:40 -0300

    first_class

---
+++ 01_string_manipulation/exercise_2.py
@@ -0,0 +1,4 @@
+nome = 'Leo'
+idade = 28
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
---
+++ 01_string_manipulation/exercise_4.py
@@ -0,0 +1,4 @@
+pi = 3.14159
+
+print(f'O valor arredondado de pi é: {round(pi, 2)}') #This is what I proposed
+print(f'O valor arredondado de pi é: {pi:.2f}') #that's the instructure opinion 
